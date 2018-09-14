a9f1201d04a55bbdc9905d74143de53ae37222d7
Arturo Garcia <arturo.gfuentes@hotmail.com>
2018-09-13 20:15:28 -0500

    Second Commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hola mundo!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//console.log('hola mundo!');
+
+const app = document.getElementById('app');
+//ReactDOM.render(que voy a renderizar, donde lo hare)
+const holaMundo = <h1>HolaMundo</h1>;
+ReactDOM.render(holaMundo,app);
